c6228540f98704e5e9a6949d2cec4d8c2fa3001f
naufalharitsprasetia <naufalharisprasetia@gmail.com>
2025-09-23 22:38:36 +0700

    file upload menit ke 11

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -4,11 +4,17 @@ namespace App\Livewire;
 
 use App\Models\User;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 use Livewire\Attributes\Validate;
 use Illuminate\Support\Facades\Hash;
 
 class Users extends Component
 {
+    use WithFileUploads;
+
+    #[Validate('image|max:5120')] // 1MB Max
+    public $avatar;
+
     public $titles = "Ini Data Title";
     public $users = '';
 
@@ -23,12 +29,19 @@ class Users extends Component
 
     public function createNewUser()
     {
-        $this->validate();
+        $validated = $this->validate();
+
+        if ($this->avatar) {
+            $validated['avatar'] = $this->avatar->store('avatar', 'public');
+        }
+
         User::create([
             'name' => $this->name,
             'email' =>  $this->email,
             'password' => Hash::make($this->password),
+            'avatar' => $validated['avatar']
         ]);
+
         session()->flash('success', 'User Baru Telah Ditambahkan!');
         $this->reset();
     }
